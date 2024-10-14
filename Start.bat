aa44a68bc3e9d0383afc7e5a132c9824362e2101
Yvan57 <71009312+Yvan57@users.noreply.github.com>
2024-10-15 02:17:21 +0300

    Delete Start.bat
    Обновление работы V2ray_updater. Переход на один файл с возможностью выбора действий.
    /
    Updating the operation of V2ray_updater. Switch to single file with selectable actions.

--- Start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%~1"=="" (
-    echo Пожалуйста, перетащите файл update_profiles.py на этот .bat файл.
-    pause
-    exit /b
-)
-cd /d "%~dp1"
-python update_profiles.py profiles.txt
-pause
