8ae5c35e2b9edb3d06d9671476ab2f2e895d8da3
Yvan57 <71009312+Yvan57@users.noreply.github.com>
2024-11-05 04:32:02 +0300

    Delete PIPInstall.bat
    v1.6 -> v1.7

--- PIPInstall.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing required Python packages...
-
-pip install requests -v
-pip install pyperclip -v
-pip install colorama -v
-
-echo All packages have been installed successfully!
-pause
